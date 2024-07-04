ee0505749b4a9556bf577b8cdd7ddfdaea9f8297
dharmil1953 <dharmilkotak00@gmail.com>
2024-07-04 14:17:08 +0530

    checking

--- src/components/BlogListingSection/index.tsx
+++ src/components/BlogListingSection/index.tsx
@@ -8,7 +8,7 @@ const BlogListingSection: React.FC<BlogListingSectionType> = (block) => {
     <div className="py-[100px] bg-theme-ivory">
       <div className="container">
         {title && (
-          <h2 className="s(767):text-32 s(1280):text-42 text-52 font-medium leading-[1] text-theme-black s(1100):pb-[32px] pb-[52px]">
+          <h2 className="s(766):text-32 s(1280):text-42 text-52 font-medium leading-[1] text-theme-black s(1100):pb-[32px] pb-[52px]">
             {title}
           </h2>
         )}
